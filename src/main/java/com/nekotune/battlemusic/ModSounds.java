648f454388a04d135826d3b5437ef107463389fa
Niko <43053729+NovaN4@users.noreply.github.com>
2024-06-16 11:55:31 -0700

    Make ModSounds abstract

--- src/main/java/com/nekotune/battlemusic/ModSounds.java
+++ src/main/java/com/nekotune/battlemusic/ModSounds.java
@@ -7,7 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public class ModSounds
+public abstract class ModSounds
 {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
             DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, BattleMusic.MOD_ID);
